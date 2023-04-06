e2b4bfa1cf2071e5662f05fb9fcc3031691cd392
倚楼听风雨 <81612992+RUInalan@users.noreply.github.com>
2023-04-06 17:04:30 +0800

    Update 脚本文件.js

--- 脚本文件.js
+++ 脚本文件.js
@@ -8,17 +8,20 @@
 // @match             *://adzhp.net/*
 // ==/UserScript==
 
-(function () {
+window.onload = (function () {
 
-    function StraightChainP(){
+    function StraightChainP() {
+        console.log("ks");
+
         // 获取页面上所有的 <a> 标签
         const links = document.getElementsByTagName('a');
 
         // 遍历每个链接，匹配与正则表达式相符的链接
         for (let i = 0; i < links.length; i++) {
             const link = links[i];
-            const pattern = /<a href="([^"]+)"\s+target="_blank"\s+data-id="(\d+)"\s+data-url="([^"]+)"\s+class="card mb-3 site-(\d+)"\s+title="([^"]+)">/;
+            const pattern = /<a\s+href="([^"]+)"\s+target="_blank"\s+data-id="(\d+)"\s+data-url="([^"]+)"\s+class="[^"]*site-(\d+)"\s+title="([^"]+)">/g;
             if (pattern.test(link.outerHTML)) {
+                console.log(link);
                 //开始修改
                 link.href = link.dataset.url;
             }
@@ -27,11 +30,16 @@
 
 
     StraightChainP();
+    console.log("ks1166");
+    Other();
+    function Other(){
+        var pagenumber = document.querySelectorAll(".pagenumber.nav-item");
+        for (let i = 0; i < pagenumber.length; i++) {
+            pagenumber[i].onmouseenter = StraightChainP;
+            var anchorElement = pagenumber[i].querySelector("a");
+            anchorElement.onmouseenter = StraightChainP;
+        }
+    }
 
-   const pagenumber = document.getElementsByClassName("pagenumber nav-item");
-   for (let i = 0; i < pagenumber.length; i++) {
-        console.log(pagenumber[i]);
-        pagenumber[i].onclick = StraightChainP;
-   }
 
 })();
